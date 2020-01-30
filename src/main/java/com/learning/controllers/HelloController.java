7964c679b3054a95fd405c95075497a3335f5a93
cabhishy <cabhishy@akamai.com>
2020-01-30 22:13:03 +0530

    Sample HelloController for Testing

---
+++ src/main/java/com/learning/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.learning.controllers;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String respondHello(){
+        return "Hello";
+    }
+}
---
+++ src/main/java/com/learning/dbconnectors/CassandraConnector.java
@@ -0,0 +1,5 @@
+package com.learning.dbconnectors;
+
+public class CassandraConnector {
+
+}
